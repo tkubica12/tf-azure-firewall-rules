bc7bfda643a1f3117f62acebe1f0bcdfa092b020
Tomas Kubica <tkubica12@gmail.com>
2022-10-18 07:51:14 +0200

    tf init with OIDC federation

--- terraform/ip_groups.tf
+++ terraform/ip_groups.tf
@@ -1,10 +1,3 @@
-resource "azurerm_ip_group" "onprem" {
-  name                = "onprem"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  cidrs               = ["10.99.0.0/16", "192.168.0.0/18"]
-}
-
 resource "azurerm_ip_group" "projects" {
   for_each            = yamldecode(file("../rules/metadata.yaml"))["projects"]
   name                = each.key
@@ -13,7 +6,3 @@ resource "azurerm_ip_group" "projects" {
   cidrs               = each.value["ranges"]
 }
 
-
-
-
-# for_each         = fileset(path.module, "subscriptions/*.yaml")
